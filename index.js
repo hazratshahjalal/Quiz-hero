9586a7a673b742100f7bb4155cd0c99e7ebd5fd3
hazratshahjalal <hazratshahjalal503@gmail.com>
2023-03-15 23:23:24 -0500

    event Listener spelling mistake solve

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
+document.querySelector("#submit").addEventListener("click", () => {
   if (answers.length < 6) {
     return;
   }
@@ -182,3 +182,4 @@ document.querySelector("#submit").addEventlistener("click", () => {
   }, 1500);
   window.scrollTo(0, 0);
 });
+
